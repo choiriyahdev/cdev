e979f90b0281c5d1d7740dc057fff66bfd2bd418
kenblockchainlabs <126150587+kenblockchainlabs@users.noreply.github.com>
2023-02-25 12:03:24 +0700

    Create console.js

---
+++ console.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>kenblockchainlabs Javascript</title>
+</head>
+<body>
+    <script>
+        console.log("wagmi");
+        document.write("bakme");
+    </script>
+</body>
+</html>
